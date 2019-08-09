2b67aff3f491e2981b1461b3737a1ba2b6eb793f
Daniel Morandini <danielmorandini@me.com>
2019-08-09 15:45:56 +0200

    Rename encoder package to csv

---
+++ csv/encoder.go
@@ -0,0 +1,54 @@
+// Copyright © 2019 booster authors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package csv
+
+import (
+	"encoding/csv"
+	"io"
+
+	"github.com/booster-proj/lsaddr/lookup"
+)
+
+// Encoder returns an Encoder which encodes a list
+// of NetFile into CSV format.
+type Encoder struct {
+	w *csv.Writer
+}
+
+func NewEncoder(w io.Writer) *Encoder {
+	return &Encoder{
+		w: csv.NewWriter(w),
+	}
+}
+
+// Encode writes `l` into encoder's writer in CSV format. Some data may have been
+// written to the writer even upon error.
+func (e *Encoder) Encode(l []lookup.NetFile) error {
+	header := []string{"COMMAND", "NET", "SRC", "DST"}
+	if err := e.w.Write(header); err != nil {
+		return err
+	}
+
+	for _, v := range l {
+		record := []string{v.Command, v.Src.Network(), v.Src.String(), v.Dst.String()}
+		if err := e.w.Write(record); err != nil {
+			return err
+		}
+	}
+
+	e.w.Flush()
+	return e.w.Error()
+}
---
+++ csv/encoder_test.go
@@ -0,0 +1,54 @@
+// Copyright © 2019 booster authors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package csv_test
+
+import (
+	"net"
+	"strings"
+	"testing"
+
+	"github.com/booster-proj/lsaddr/csv"
+	"github.com/booster-proj/lsaddr/lookup"
+)
+
+func TestEncode_CSV(t *testing.T) {
+	l := netFiles0
+	var w strings.Builder
+	if err := csv.NewEncoder(&w).Encode(l); err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+
+	expOut := `COMMAND,NET,SRC,DST
+foo,udp,192.168.0.61:54104,52.94.218.7:443
+bar,udp,[::1]:60051,[::1]:60052
+`
+	if expOut != w.String() {
+		t.Fatalf("Unexpected output: wanted \"%s\", found \"%s\"", expOut, w.String())
+	}
+}
+
+var netFiles0 = []lookup.NetFile{
+	{"foo", newUDPAddr("192.168.0.61:54104"), newUDPAddr("52.94.218.7:443")},
+	{"bar", newUDPAddr("[::1]:60051"), newUDPAddr("[::1]:60052")},
+}
+
+func newUDPAddr(address string) net.Addr {
+	addr, err := net.ResolveUDPAddr("udp", address)
+	if err != nil {
+		panic(err)
+	}
+	return addr
+}
